9eee0e83a972c45b1e5a6f9c9ec5d46a84415f82
Arlingor <iskren93@gmail.com>
2018-04-15 22:14:33 +0300

    ccc

--- js/content.js
+++ js/content.js
@@ -1,6 +1,17 @@
-function drop() {
-    
-    return document.querySelector("#dropdownmenu").style.display="block";
 
 
+// Dropdown Functions
+function navdrop() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
+function drop1() {
+        document.getElementById("drop1").classList.toggle("show");
+}
+function drop2() {
+    document.getElementById("drop2").classList.toggle("show");
+}
+function drop3() {
+    document.getElementById("drop3").classList.toggle("show");
+}
+//---------------------
+
