f7a19fba7b62c5d1a5ce0efe0c82094d13ad6e74
Iskren <iskren93@gmail.com>
2018-04-16 15:18:22 +0300

    commit

--- js/content.js
+++ js/content.js
@@ -9,4 +9,17 @@ function drop3() {
     document.getElementById("drop3").classList.toggle("show");
 }
 //---------------------
+// Function to close dropdown manus
+var dropdown = document.getElementById("drop1");
+var dropdown2 = document.getElementById("drop2");
+var dropdown3 = document.getElementById("drop3");
 
+window.onclick = function(e) {
+    if (!e.target.matches('#click')) {        
+         if(dropdown.classList.contains("show") || dropdown2.classList.contains("show") || dropdown3.classList.contains("show") ){
+             dropdown.classList.remove("show");
+             dropdown2.classList.remove("show");
+             dropdown3.classList.remove("show");
+         }
+    }
+  }
